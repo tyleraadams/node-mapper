0eab6853ca748edd3325e5a888eefb67e3ca572f
Tyler Adams <tyleraadams@gmail.com>
2015-05-24 01:18:19 -0400

    data geo takes precedence over data location; manipualting data location data to average the bounding box coordinates

--- index.js
+++ index.js
@@ -28,29 +28,45 @@ io.on('connection', function(socket){
 	// var searchTerm = req.query.search;
   //io.on('connection', function(socket){
 	socket.on('search', function(searchTerm){
- 		console.log('!! ', searchTerm);
-    	twit.stream('statuses/filter', { track: searchTerm.search,  'locations':'-180,-90,180,90'}, function(stream){
+    	twit.stream('statuses/filter', { track: searchTerm.search}, function(stream){
     	// streamHandler(stream,io);
     		twit.currentTwitStream = stream;
     		twit.currentTwitStream.on('data', function(data) {
 
 		    // Construct a new tweet object
-
+		    //console.log(data);
 		    var tweet = {
 		      twid: data['id'],
-		      active: false,
 		      author: data['user']['name'],
 		      avatar: data['user']['profile_image_url'],
 		      body: data['text'],
 		      date: data['created_at'],
 		      screenname: data['user']['screen_name'],
 		    };
+		    //console.log('does it contain the search term?   ', tweet.body.indexOf(searchTerm) > -1)
+			// need to make a case for location
+			if (data['place']) {
+				//console.log ('LOCATION: ', data['place'].bounding_box.coordinates[0][0]);
+				var location =  data['place'].bounding_box.coordinates[0];
+				var avg1 = 0;
+				var avg2 = 0;
+				for(var i =0; i < location.length; i++) {
+				  avg1+=location[i][0];
+				  avg2+=location[i][1];
+				  if (i === location.length - 1) { avg1 = avg1/location.length; avg2 = avg2/location.length; }
+				}
+				tweet['location'] = [avg2, avg1]; //location.map(function(val,index){if(index === 0){return location[index+1]} else {return location[index-1]}});
+			}
 
-		    if (data['geo']) {
+			if (data['geo']) {
 		      tweet['location'] = data['geo']['coordinates'];
 		    }
 
-		    io.emit('tweet', tweet);
+		    if (tweet['location']) {
+		    	io.emit('tweet', tweet);
+		    }
+
+
   		});
 
 
